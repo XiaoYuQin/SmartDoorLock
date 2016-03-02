ec8c4030a1095c86d5743abd9c340bbf13db70b0
秦晓宇 <qinxiaoyu@163.com>
2016-03-02 10:06:41 +0800

    根据资料添加了serialport和相应的打开关闭方法等，再解决点问题就能读写串口数据了

--- application/SmartDoor/SmartDoor/Form1.Designer.vb
+++ application/SmartDoor/SmartDoor/Form1.Designer.vb
@@ -22,9 +22,128 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.portnamebox = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.baudratebox = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.openbtn = New System.Windows.Forms.Button()
+        Me.closebtn = New System.Windows.Forms.Button()
+        Me.statuslabel = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'portnamebox
+        '
+        Me.portnamebox.FormattingEnabled = True
+        Me.portnamebox.Location = New System.Drawing.Point(56, 28)
+        Me.portnamebox.Name = "portnamebox"
+        Me.portnamebox.Size = New System.Drawing.Size(121, 20)
+        Me.portnamebox.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "串口"
+        '
+        'baudratebox
+        '
+        Me.baudratebox.FormattingEnabled = True
+        Me.baudratebox.Items.AddRange(New Object() {"9600", "38400", "115200"})
+        Me.baudratebox.Location = New System.Drawing.Point(56, 66)
+        Me.baudratebox.Name = "baudratebox"
+        Me.baudratebox.Size = New System.Drawing.Size(121, 20)
+        Me.baudratebox.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "波特率"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 12)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "串口状态"
+        '
+        'openbtn
+        '
+        Me.openbtn.Location = New System.Drawing.Point(205, 28)
+        Me.openbtn.Name = "openbtn"
+        Me.openbtn.Size = New System.Drawing.Size(75, 23)
+        Me.openbtn.TabIndex = 5
+        Me.openbtn.Text = "连接串口"
+        Me.openbtn.UseVisualStyleBackColor = True
+        '
+        'closebtn
+        '
+        Me.closebtn.Location = New System.Drawing.Point(205, 66)
+        Me.closebtn.Name = "closebtn"
+        Me.closebtn.Size = New System.Drawing.Size(75, 23)
+        Me.closebtn.TabIndex = 6
+        Me.closebtn.Text = "断开串口"
+        Me.closebtn.UseVisualStyleBackColor = True
+        '
+        'statuslabel
+        '
+        Me.statuslabel.AutoSize = True
+        Me.statuslabel.Location = New System.Drawing.Point(71, 109)
+        Me.statuslabel.Name = "statuslabel"
+        Me.statuslabel.Size = New System.Drawing.Size(53, 12)
+        Me.statuslabel.TabIndex = 7
+        Me.statuslabel.Text = "串口状态"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(205, 104)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "发送数据"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 324)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.statuslabel)
+        Me.Controls.Add(Me.closebtn)
+        Me.Controls.Add(Me.openbtn)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.baudratebox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.portnamebox)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents portnamebox As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents baudratebox As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents openbtn As System.Windows.Forms.Button
+    Friend WithEvents closebtn As System.Windows.Forms.Button
+    Friend WithEvents statuslabel As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
 
 End Class
--- application/SmartDoor/SmartDoor/Form1.vb
+++ application/SmartDoor/SmartDoor/Form1.vb
@@ -1,3 +1,77 @@
-﻿Public Class Form1
+﻿Imports System
+Imports System.IO.Ports
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '获取计算机有效串口
+        Dim ports As String() = SerialPort.GetPortNames() '必须用命名空间，用SerialPort,获取计算机的有效串口
+        Dim port As String
+        For Each port In ports
+            portnamebox.Items.Add(port) '向combobox中添加项
+        Next port
+        baudratebox.SelectedIndex() = 0
+        portnamebox.SelectedIndex() = 0
+        statuslabel.Text = "串口未连接"
+        statuslabel.ForeColor = Color.Red
+
+    End Sub
+
+    Private Sub Serial_Port1() '设置串口参数
+        SerialPort1.BaudRate = Val(baudratebox.Text) '波特率
+        SerialPort1.PortName = portnamebox.Text '串口名称
+        SerialPort1.DataBits = 8 '数据位
+        SerialPort1.StopBits = IO.Ports.StopBits.One '停止位
+        SerialPort1.Parity = IO.Ports.Parity.None '校验位
+    End Sub
+
+
+    Private Sub openbtn_Click(sender As Object, e As EventArgs) Handles openbtn.Click
+        Try
+            SerialPort1.Open() '打开串口
+            Label3.Text = SerialPort1.IsOpen
+            If SerialPort1.IsOpen = True Then
+                statuslabel.Text = "串口已连接"
+                statuslabel.ForeColor = Color.Green
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub closebtn_Click(sender As Object, e As EventArgs) Handles closebtn.Click
+        Try
+            SerialPort1.Close() '关闭串口
+            Label3.Text = SerialPort1.IsOpen
+            If SerialPort1.IsOpen = False Then
+                statuslabel.Text = "串口未连接"
+                statuslabel.ForeColor = Color.Red
+                receivebox.Text = ""
+                receivebytes.Text = ""
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '触发接收事件
+    Public Sub Sp_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Me.Invoke(New EventHandler(AddressOf Sp_Receiving)) '调用接收数据函数
+    End Sub
+
+    '接收数据
+    Private Sub Sp_Receiving(ByVal sender As Object, ByVal e As EventArgs)
+        Dim strIncoming As String
+        Try
+            receivebytes.Text = Str(Val(receivebytes.Text) + SerialPort1.BytesToRead)
+            If SerialPort1.BytesToRead > 0 Then
+                Threading.Thread.Sleep(100) '添加的延时
+                strIncoming = SerialPort1.ReadExisting.ToString '读取缓冲区中的数据
+                SerialPort1.DiscardInBuffer()
+                receivebox.Text = strIncoming
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
 
 End Class
