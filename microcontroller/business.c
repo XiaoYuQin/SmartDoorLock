5c4b83c475d9802a9c2254d966a71ea585eaddab
QinXiaoyuAsus <qinxiaoyu@163.com>
2016-03-26 01:13:42 +0800

    单片机软件完成

--- microcontroller/business.c
+++ microcontroller/business.c
@@ -5,11 +5,18 @@
 #include <stdio.h>
 #include <reg52.h>
 
+#define LOCKER_TIMER 7
 
 extern app_Event getApp;
 
 extern uint8 uartData[20];
 extern uint8 uartIndex;
+
+sbit wela2=P1^1;	//定义位选
+int timer;
+uint8 code table[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};//不带小数点的共阴数码管段值
+sbit lockerPin=P1^7;	//定义位选
+
 
 typedef enum{
 	LOCK,
@@ -17,6 +24,8 @@ typedef enum{
 }LOCKER_STATUS;
 
 LOCKER_STATUS lockerStatus = LOCK;
+
+
 
 void userBusinessEventHandle()
 {
@@ -38,9 +47,9 @@ void userBusinessEventHandle()
 				break;
 				case EVENT_LOCK_LOCKER:
 				{
-					lockerStatus = LOCK;
+					lockerStatus = UNLOCK;
 					reportLockerStatus();
-					uartSendStr("lock locker\r\n");
+					uartSendStr("UNLOCK\r\n");
 				}
 				break;
 				case EVENT_LOCKER_STATUS_RSP:
@@ -49,6 +58,41 @@ void userBusinessEventHandle()
 					setEvent(EVENT_LOCKER_STATUS_RSP, ENABLE,300,1);
 				}
 				break;
+				/*锁倒计时*/
+				case EVENT_LOCKER_DAOJISHI:
+				{
+					
+			
+					P0=table[timer];	//传递数据
+					P1 |= 0x01;		//打开位选
+					P1 &= 0xF9;
+					wela2 = 0;	   //关闭位选
+					timer = timer -1;
+						
+
+					if(timer < -1)
+					{
+						timer =LOCKER_TIMER;
+						setEvent(EVENT_LOCKER_LOCKED, ENABLE,1,1);
+						setEvent(EVENT_LOCKER_DAOJISHI, DISABLE,0,0);
+						lockerStatus = LOCK;
+						reportLockerStatus();
+					}
+					else
+					{					
+						uartSendStr("EVENT_LOCKER_DAOJISHI\r\n");
+						setEvent(EVENT_LOCKER_DAOJISHI, ENABLE,200,1);
+						lockerStatus = UNLOCK;
+						reportLockerStatus();						
+					}
+				}
+				break;
+				case EVENT_LOCKER_LOCKED:
+					P0 =0x40;	//传递数据
+					P1 |= 0x01;		//打开位选
+					P1 &= 0xF9;
+					wela2 = 0;	   //关闭位选
+					break;
 				default:
 					break;
 			
@@ -66,16 +110,33 @@ void dealLocker(uint8 *cmd)
 	if(uartIndex == 0) return;
 	
 	if((cmd[0] == 'U')&&(cmd[1] == 'N')&&cmd[2] == 'L'&&cmd[3] == 'O'&&cmd[4] == 'C'&&cmd[5] == 'K'){
+		lockerPin = 1;
+		timer = LOCKER_TIMER;
 		uartSendStr("open locker 5s\r\n");
 		lockerStatus = UNLOCK;
 		reportLockerStatus();
-		setEvent(EVENT_LOCK_LOCKER, ENABLE,500,1);
+		//setEvent(EVENT_LOCK_LOCKER, ENABLE,500,1);
+		setEvent(EVENT_LOCKER_DAOJISHI, ENABLE,1,1);
+	}
+	if((cmd[0] == 'T')&&(cmd[1] == 'O')&&cmd[2] == 'L'&&cmd[3] == 'O'&&cmd[4] == 'C'&&cmd[5] == 'K'){
+
+		timer = 0;
+		lockerStatus = LOCK;
+		reportLockerStatus();
+		setEvent(EVENT_LOCKER_LOCKED, ENABLE,1,1);
+		setEvent(EVENT_LOCKER_DAOJISHI, DISABLE,0,0);
 	}
 }
 void reportLockerStatus()
 {
 	if(lockerStatus == UNLOCK)
+	{
+		lockerPin = 1;
 		uartSendStr("unlock\r\n");
+	}
 	else
+	{
+		lockerPin = 0;
 		uartSendStr("lock\r\n");
+	}
 }
--- microcontroller/main.c
+++ microcontroller/main.c
@@ -4,29 +4,54 @@
 #include "user_sys.h"
 #include "business.h"
 
-sbit LED1=P1^7;//声明端口
+
+
+
+sbit lockerPin=P1^7;	//定义位选
+
 
 void init()
 {
+
+	lockerPin = 0;
+	
+	Init_Timer0();
+
 	InitUART();	
 	uartSendStr("int..\r\n");
 	//Init_Timer0();
-	TIM2Inital();
+//	TIM2Inital();
+
 	userSystemEventInit();		
 	uartSendStr("int end...\r\n");	
+
 }
 void main()
 { 
+//uint i; 
   	init();
 
-	LED1  = 0; //置P1_7口为低电平 ,点亮LED
+	
+	
+	//LED1  = 0; //置P1_7口为低电平 ,点亮LED
  		
 	setEvent(EVENT_UART_RECIVE, ENABLE,30,1);
 	setEvent(EVENT_LOCKER_STATUS_RSP, ENABLE,300,1);
-	uartDataClean();
+	setEvent(EVENT_LOCKER_LOCKED, ENABLE,1,1);
+
+	uartDataClean();	
 	while(1)
 	{
+		/*for(i=0;i<10;i++)	  
+		{
+			P0=table[i];	//传递数据
+			P1 = 0x01;		//打开位选
+			delayms(5);
+			wela2 = 0;	   //关闭位选
+			delayms(1000); //延时1S
+		}	 */
 		userBusinessEventHandle();
+
 	}
 }
 
