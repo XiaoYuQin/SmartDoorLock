83f763809698b2eb590cbe9d4f557d5030f1b0f5
QinXiaoyuAsus <qinxiaoyu@163.com>
2016-03-28 23:23:55 +0800

    智能锁PC端软件完成，删掉了原来的VB方案，使用C#方案实现

---
+++ application/eHealth/eHealth/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace eHealth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gpsStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialBox
+            // 
+            this.serialBox.FormattingEnabled = true;
+            this.serialBox.Location = new System.Drawing.Point(70, 27);
+            this.serialBox.Name = "serialBox";
+            this.serialBox.Size = new System.Drawing.Size(75, 20);
+            this.serialBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "端口号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "锁状态";
+            // 
+            // gpsStatus
+            // 
+            this.gpsStatus.AutoSize = true;
+            this.gpsStatus.Location = new System.Drawing.Point(13, 69);
+            this.gpsStatus.Name = "gpsStatus";
+            this.gpsStatus.Size = new System.Drawing.Size(0, 12);
+            this.gpsStatus.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(65, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 26);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Not locate";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "关闭";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 113);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 177);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(177, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 44);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "开锁";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 150);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gpsStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.serialBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "智能锁";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox serialBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label gpsStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ application/eHealth/eHealth/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.IO.Ports;
+
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace eHealth
+{
+    public partial class Form1 : Form
+    {
+        SerialPort com;
+        bool isConnect;
+       // delegate void HandleInterfaceUpdateDelegate(string text);  //委托，此为重点
+       // HandleInterfaceUpdateDelegate interfaceUpdateHandle;
+        Thread _readThread;
+        bool _keepReading;
+
+
+
+
+        enum LOCKER_STATUS{
+            LOCK,
+            UNLOCK
+        };
+
+        LOCKER_STATUS status;
+
+        private System.Timers.Timer timer = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            com = new SerialPort();
+            isConnect = false;
+            
+
+            //timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            //timer.Enabled = true;
+
+ 
+            Console.WriteLine("serial port");
+
+            String[] str = SerialPort.GetPortNames();
+            if (str == null)
+            {
+                MessageBox.Show("no port ", "error");
+                return;
+            }
+            foreach (String s in SerialPort.GetPortNames())
+            {
+                System.Console.WriteLine(s);
+                serialBox.Items.Add(s);
+            }
+            serialBox.SelectedIndex = 0;
+            button1.Text = "打开串口";
+            label5.Text = "...";
+            button3.Text = "...";
+           // interfaceUpdateHandle = new HandleInterfaceUpdateDelegate(UpdateTextBox);  //实例化委托对象
+            //this.com.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.OnDataRecivice);
+            com.ReceivedBytesThreshold = 1;
+
+    
+
+
+        }
+        private void OnDataRecivice(object sender ,SerialDataReceivedEventArgs e) {
+            byte[] readBuffer = new byte[com.ReadBufferSize];
+            com.Read(readBuffer, 0, readBuffer.Length);
+            //this.Invoke(interfaceUpdateHandle, new string[] { Encoding.Unicode.GetString(readBuffer) });
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isConnect == false)
+            {
+                try
+                {
+                    com.BaudRate = 9600;
+                    Console.WriteLine(serialBox.Text);
+                    com.PortName = serialBox.Text;
+                    com.DataBits = 8;
+                    com.Open();
+                    isConnect = true;
+                    _keepReading = true;
+                    _readThread = new Thread(ReadPort);
+                    _readThread.Start();
+                    button1.Text = "关闭串口";
+                }
+                catch (System.IO.IOException) {
+                    button1.Text = "打开串口";
+                    isConnect = false;
+                    MessageBox.Show("打开串口失败", "失败");                
+                }
+                catch (System.NullReferenceException){
+                    button1.Text = "打开串口";
+                    isConnect = false;
+                    MessageBox.Show("打开串口失败", "失败");
+                }
+            }
+            else {
+                _keepReading = false;
+                isConnect = false;
+                com.Close();
+                button1.Text = "打开串口";
+            }
+        }
+
+        /*private void button2_Click(object sender, EventArgs e)
+        {
+            com.WriteLine("1234");
+            Console.WriteLine("1234");
+        }*/
+        private void UpdateTextBox(string text)
+        {
+            Console.WriteLine(text);
+            
+        }
+        private void ReadPort() {
+            while (_keepReading) {
+
+                if (isConnect == true){
+
+                    //byte[] readBuffer = new byte[com.ReadBufferSize + 1];
+                    try
+                    {
+
+                        String SerialIn = com.ReadLine();
+                        Console.WriteLine(SerialIn);
+
+                        String sssss = SerialIn;
+                        textBox1.Text = textBox1.Text + sssss + "\n\r";
+                        textBox1.SelectionStart = textBox1.Text.Length - 1;
+                        textBox1.ScrollToCaret();
+                        //sssss = "AT+BT=60\r\n";
+
+                        if (sssss == "lock\r") {
+                            Console.WriteLine("锁关闭");
+                            label5.Text = "锁关闭";
+                            label5.BackColor = Color.Red;
+                            status = LOCKER_STATUS.LOCK;
+                            button3.Text = "打开";
+                        }
+                        else if(sssss == "unlock\r"){
+                            Console.WriteLine("锁打开");
+                            label5.Text = "锁打开";
+                            label5.BackColor = Color.Green;
+                            status = LOCKER_STATUS.UNLOCK;
+                            button3.Text = "关闭";
+                        }
+                  
+
+                        //HttpPost(value, longitude, latitude);
+
+                    }
+                    catch (TimeoutException) { }
+                    catch (IOException) { }
+                }
+                else {
+                    TimeSpan waitTime = new TimeSpan(0,0,0,50);
+                    Thread.Sleep(waitTime);
+                }
+            }
+        }
+        
+
+
+
+
+       /* private void button2_Click(object sender, EventArgs e)
+        {
+            //AT+ST=1
+            //AT+BT=123
+            //AT+LO=1234
+            //AT+LA=123
+            com.WriteLine("1234");
+            Console.WriteLine("1234");
+        }*/
+
+  
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isConnect == true) {
+                _keepReading = false;
+                com.Close();
+                isConnect = false;
+            }
+            this.Close();
+            /*com.WriteLine("1234");
+            Console.WriteLine("1234");*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (status == LOCKER_STATUS.LOCK)
+            {
+                button3.Text = "打开";
+                com.WriteLine("UNLOCK");
+            }
+            else 
+            {
+                button3.Text = "关闭";
+                com.WriteLine("TOLOCK");
+            }
+        }
+
+        /*void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //模拟的做一些耗时的操作
+            //Console.WriteLine("1234");
+            HttpPost();
+            System.Threading.Thread.Sleep(1000000);
+            System.Threading.Thread.Sleep(1000000);
+        }*/
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (DialogResult.OK == MessageBox.Show("你确定要关闭应用程序吗？", "关闭提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Question))
+            {
+                //this.FormClosing -= new FormClosingEventHandler(this.Form1_FormClosing);//为保证Application.Exit();时不再弹出提示，所以将FormClosing事件取消
+                Application.Exit();//退出整个应用程序
+            }
+            else
+            {
+                e.Cancel = true;  //取消关闭事件
+            }
+        }
+
+    }
+}
